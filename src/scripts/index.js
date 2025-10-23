aeab015a3fd1a5042ddd2d767eae8c49b7cf03f2
Akbar Noto PB <akbarnotopb@gmail.com>
2025-10-23 10:37:09 +0700

    update views

---
+++ src/scripts/index.js
@@ -0,0 +1,43 @@
+let timeout = null
+$(document).ready(function(){
+    if(!(screen.width < 600 || (screen.height<600 && window.orientation != 0))){
+        if($(window).scrollTop() !== 0){
+            clearTimeout(timeout)
+            $(".left-intro").width("100%");
+            $(".right-intro").width("0%");
+            $("#intro").addClass("lefted");
+        }else{
+            $(".left-intro").width("50%");
+            timeout = setTimeout(()=>{
+                $(".right-intro").width("50%");
+            },1800)
+            $("#intro").removeClass("lefted");
+        }
+    }
+})
+
+
+$(window).scroll(function(){
+    if(screen.width < 600 || (screen.height<600 && window.orientation != 0)){
+        return
+    }
+
+    if($(window).scrollTop() !== 0){
+        clearTimeout(timeout)
+        $(".left-intro").width("100%");
+        $(".right-intro").width("0%");
+        $("#intro").addClass("lefted");
+        if($(window).scrollTop() >= window.innerHeight * 0.4){
+            $("#fading-scroll-helper").animate({"opacity": 0});
+        }
+    }else{
+        if($(window).scrollTop() < window.innerHeight * 0.4){
+            $("#fading-scroll-helper").animate({"opacity": 100});
+        }
+        $(".left-intro").width("50%");
+        timeout = setTimeout(()=>{
+            $(".right-intro").width("50%");
+        },1800)
+        $("#intro").removeClass("lefted");
+    }
+})
